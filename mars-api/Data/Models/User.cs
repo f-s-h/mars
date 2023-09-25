31255fca15c3babd0d40a12b727b9c2a42443ba6
fabian.heinrich <fabian.heinrich@aracom.de>
2023-09-25 13:54:06 +0200

    Added new User attributes

--- mars-api/Data/Models/User.cs
+++ mars-api/Data/Models/User.cs
@@ -4,6 +4,10 @@ namespace mars_api.Data.Models
 {
     public class User : IdentityUser
     {
-        
+        public string? Title { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateOnly? Birthday { get; set; }
+
     }
 }
