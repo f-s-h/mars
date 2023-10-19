cdbfd570f7d6f6ae09bb592c12b83be24faa2d8e
f-s-h <fabian.heinrich@tum.de>
2023-10-19 10:27:11 +0200

    Merge block

---
+++ mars-api/mars-api/Data/Models/Users/EMail.cs
@@ -0,0 +1,15 @@
+﻿namespace mars_api.Data.Models.Users
+{
+    public class EMail
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Email { get; set; } = string.Empty;
+
+        public static Boolean IsEmailValid(string Email)
+        {
+            // TODO
+            return false;
+        }
+    }
+}
